4a9588ee6bde13d8f5babbd594355ec1f4ca97e7
Evangelos Angelakis <167527848+EvangelosAngelakis@users.noreply.github.com>
2024-12-21 10:21:47 +0000

    Added JS to make calculator work
    Calculator now only does multiplications but will make changes to select what the user wants to do.

--- Basic_Calculator.js
+++ Basic_Calculator.js
@@ -1,3 +1,22 @@
+function multiplyValues() {
+    const value1 = parseFloat(document.getElementById('value1').value);
+    const value2 = parseFloat(document.getElementById('value2').value);
+
+    if (isNaN(value1) || isNaN(value2)) {
+        document.getElementById('result').innerText = "Please Enter Valid numbers.";
+        return;
+    }
+    
+    const result = value1 * value2;
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
+
+
+
+function pageReset(){
+    location.reload()
+};
+
 function pageReset(){
     location.reload()
 };
